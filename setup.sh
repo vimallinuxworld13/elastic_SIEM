9886ffb692fa6229a5089b029151c50a87510dc1
vimallinuxworld13 <vdaga@lwindia.com>
2025-12-13 19:33:39 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,3 +11,11 @@ sudo systemctl enable --now docker
 docker network create elastic
 
 docker run --name es01 -dit -p 9200:9200 --net elastic   docker.elastic.co/elasticsearch/elasticsearch:9.2.1
+
+docker run --name kib01  -e XPACK_ENCRYPTEDSAVEDOBJECTS_ENCRYPTIONKEY=3e54c5cd0b159bcd5f24b6f3f5650b1a -dit --net elastic -p 5601:5601 docker.elastic.co/kibana/kibana:9.2.1
+
+docker exec -it es01 /usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana
+
+docker exec -i  es01 /usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic
+
+
