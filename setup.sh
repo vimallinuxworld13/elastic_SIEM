06b16b56c5329f235916776c3aa7029b2b48e72c
vimallinuxworld13 <vdaga@lwindia.com>
2025-12-14 18:33:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,12 +20,35 @@ in AWS linux machine:
 
 docker network create elastic
 
+sysctl vm.max_map_count
+sysctl vm.max_map_count=262144
+sysctl vm.max_map_count
+
 docker run --name es01 -dit -p 9200:9200 --net elastic   docker.elastic.co/elasticsearch/elasticsearch:9.2.1
+
+docker logs -f es01
+
 
 docker run --name kib01  -e XPACK_ENCRYPTEDSAVEDOBJECTS_ENCRYPTIONKEY=3e54c5cd0b159bcd5f24b6f3f5650b1a -dit --net elastic -p 5601:5601 docker.elastic.co/kibana/kibana:9.2.1
 
 docker exec -it es01 /usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana
 
+# docker logs kib01
+
 docker exec -i  es01 /usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic
 
+echo 3 > /proc/sys/vm/drop_caches
+
+
+
+curl -L -O https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-9.2.1-x86_64.rpm
+sudo ELASTIC_AGENT_FLAVOR=servers rpm -vi elastic-agent-9.2.1-x86_64.rpm
+sudo systemctl enable elastic-agent
+sudo systemctl start elastic-agent
+sudo elastic-agent enroll \
+  --fleet-server-es=https://172.18.0.2:9200 \
+  --fleet-server-service-token=AAEAAWVsYXN0aWMvZmxlZXQtc2VydmVyL3Rva2VuLTE3NjU3MTM5Mzg3NTM6NGpqWkJzME9SZkNxb05TWm1XZUxEUQ \
+  --fleet-server-policy=fleet-server-policy \
+  --fleet-server-es-ca-trusted-fingerprint=3e951488d30259f4b0c833d387067877194bfbcdb2bb164f9039ba5c063d551c \
+  --fleet-server-port=8220
 
